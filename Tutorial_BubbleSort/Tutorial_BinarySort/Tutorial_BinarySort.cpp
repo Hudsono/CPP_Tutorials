5a120dc4cda880400ee7e12faeee6f5e7119f3b9
Hudsono <88820344+Hudsono@users.noreply.github.com>
2022-05-04 16:53:41 +1000

    V2 BinarySearch
    A more complete implementation of BinarySearch. The Print functionality is not exactly readable but still shows the divide-and-conquer progress.

--- Tutorial_BubbleSort/Tutorial_BinarySort/Tutorial_BinarySort.cpp
+++ Tutorial_BubbleSort/Tutorial_BinarySort/Tutorial_BinarySort.cpp
@@ -4,17 +4,77 @@
 #include <iostream>
 using namespace std;
 
-void BinarySort(int* sortedList, int size);
+int BinarySearch(int* list, int size, int target);
+void PrintSearch(int l, int m, int r, int* list, int size);
 
 int main()
 {
     cout << "Hello World!\n";
 
-    const int arrSize = 9;
-    int arr[arrSize] = { 4, 6, 12, 23, 31, 45, 51, 77, 108 };
+    const int arrSize = 25;
+    int arr[arrSize] = { 4,65,63,1,54,
+        89,84,9,98,57,
+        71,18,21,84,69,
+        28,11,83,13,42,
+        64,58,78,82,13 };
+
+    int target = 71;
+
+    int result = BinarySearch(arr, arrSize, target);
+
+    cout << result;
 }
 
-void BinarySort(int* sortedList, int size)
+//Returns the index of a given target int within an array, if found.
+//Returns -1 if not found in the given array.
+int BinarySearch(int* list, int size, int target)
 {
+    //start with the leftmost at the start of the array, and the rightmost and the end of the array
+    int l = 0;
+    int r = size - 1;
+    int m = r / 2;
+    PrintSearch(l, m, r, list, size);
 
+    while (l <= r) {
+        //the middle of the array is calculated each time we adjust the search boundaries L and/or R.
+        m = (l + r) / 2;
+
+        //if the middle IS our target, return that. Convenient.
+        if (list[m] == target)
+            return m;
+
+        //if the middle is LESS than the target, we adjust our range to the right side by moving L to the middle + 1 (as we have already checked the middle above)
+        if (list[m] < target)
+            l = m + 1;
+
+        //if the middle is MORE than the target, we adjust our range to the left side by moving R to the middle - 1 (as we have already checked the middle above)
+        if (list[m] > target)
+            r = m - 1;
+
+        PrintSearch(l, m, r, list, size);
+    }
+
+    //If we haven't found anything, return -1.
+    return -1;
+}
+
+
+void PrintSearch(int l, int m, int r, int* list, int size)
+{
+    cout << "[";
+    for (int i = 0; i < size; i++)
+    {
+        if (i >= l && i <= r)
+            if (i == m)
+                cout << "<" << list[i] << ">";
+            else
+                cout << list[i];
+        else
+            cout << "..";
+
+        if (i + 2 <= size)
+            cout << " ";
+        else
+            cout << "]" << endl;
+    }
 }
--- Tutorial_BubbleSort/Tutorial_BubbleSort/Tutorial_BubbleSort.cpp
+++ Tutorial_BubbleSort/Tutorial_BubbleSort/Tutorial_BubbleSort.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 using namespace std;
 
+//BubbleSort
+void Task1();
 void BubbleSort(int* list, int size, bool descending);
 void PrintArray(int *list, int size);
 void Swap(int &a, int &b);
@@ -15,9 +17,40 @@ void CocktailShakerSort(int* list, int size, bool descending);
 int swaps;
 int comparisons;
 
+//BinarySearch
+void Task2();
+int BinarySearch(int* list, int size, int target);
+void PrintSearch(int l, int m, int r, int* list, int size);
+
 int main()
 {
-    cout << "Hello World!\n";
+    Task1();
+    Task2();
+}
+
+//Binary searching initialisation.
+void Task2()
+{
+    cout << "BinarySearch" << endl;
+
+    const int arrSize = 25;
+    int arr[arrSize] = { 4,65,63,1,54,
+        89,84,9,98,57,
+        71,18,21,84,69,
+        28,11,83,13,42,
+        64,58,78,82,13 };
+
+    int target = 71;
+
+    int result = BinarySearch(arr, arrSize, target);
+
+    cout << result;
+}
+
+//Bubble sorting initialisation.
+void Task1()
+{
+    cout << "BubbleSort" << endl;
 
     const int array_size = 50;
     int array_to_be_sorted[array_size] = {
@@ -49,6 +82,62 @@ int main()
 
     cout << "Swaps = " << swaps << endl;
     cout << "Comparisons = " << comparisons << endl;
+
+
+}
+
+//Returns the index of a given target int within an array, if found.
+//Returns -1 if not found in the given array.
+int BinarySearch(int* list, int size, int target)
+{
+    //start with the leftmost at the start of the array, and the rightmost and the end of the array
+    int l = 0;
+    int r = size - 1;
+    int m = r / 2;
+    PrintSearch(l, m, r, list, size);
+
+    while (l <= r) {
+        //the middle of the array is calculated each time we adjust the search boundaries L and/or R.
+        m = (l + r) / 2;
+
+        //if the middle IS our target, return that. Convenient.
+        if (list[m] == target)
+            return m;
+
+        //if the middle is LESS than the target, we adjust our range to the right side by moving L to the middle + 1 (as we have already checked the middle above)
+        if (list[m] < target)
+            l = m + 1;
+
+        //if the middle is MORE than the target, we adjust our range to the left side by moving R to the middle - 1 (as we have already checked the middle above)
+        if (list[m] > target)
+            r = m - 1;
+
+        PrintSearch(l, m, r, list, size);
+    }
+
+    //If we haven't found anything, return -1.
+    return -1;
+}
+
+//Print the search results in a readable format.
+void PrintSearch(int l, int m, int r, int* list, int size)
+{
+    cout << "[";
+    for (int i = 0; i < size; i++)
+    {
+        if (i >= l && i <= r)
+            if (i == m)
+                cout << "<" << list[i] << ">";
+            else
+                cout << list[i];
+        else
+            cout << "..";
+
+        if (i + 2 <= size)
+            cout << " ";
+        else
+            cout << "]" << endl;
+    }
 }
 
 //Sort a C array of integers in ascending or descending order.
